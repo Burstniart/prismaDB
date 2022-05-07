ed906cf6c6e71c787675a94cf18219fdd8814c62
Burstniart <v4gr33n@hotmail.com>
2022-05-06 23:33:15 -0500

    Add put and deleted endpoints for axolotls

--- server.js
+++ server.js
@@ -70,9 +70,29 @@ app.put('/explorers/:id', async (req, res) => {
     return res.json({message: "Actualizado correctamente"});
 });
 
+app.put('/axolotls/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    await prisma.axolotl.update({
+        where: {
+            id: id
+        },
+        data: {
+            missionCommander: req.body.missionCommander
+        }
+    })
+    return res.json({message: "Actualizado correctamente"});
+});
+
 app.delete('/explorers/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     await prisma.explorer.delete({where: {id: id}});
+    return res.json({message: "Eliminado correctamente"});
+});
+
+app.delete('/axolotls/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.axolotl``.delete({where: {id: id}});
     return res.json({message: "Eliminado correctamente"});
 });
 
